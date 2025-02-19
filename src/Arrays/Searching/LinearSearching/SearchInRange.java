fc6e7e61d55b1db8c09d0478b72d9addd956ea7b
Shiwank Kumar <shiwannnnk@gmail.com>
2025-02-19 12:03:34 +0530

    more in linear search

---
+++ src/Arrays/Searching/LinearSearching/SearchInRange.java
@@ -0,0 +1,26 @@
+package Arrays.Searching.LinearSearching;
+
+public class SearchInRange {
+
+    public static void main(String[] args) {
+
+        int[] arr = {18,12,-7,3,14,28};
+        int start = 1;
+        int end = 4;
+        int target = 3;
+
+        System.out.println("present at :- " +searching(arr,start,end,target));
+    }
+
+    static int searching(int[] arr, int start, int end, int target){
+
+        for (int i=start; i<=end; i++){
+            if (arr[i] == target){
+                return i;
+            }
+        }
+
+        return -1;
+
+    }
+}
--- src/Arrays/Searching/LinearSearching/StringSearching.java
+++ src/Arrays/Searching/LinearSearching/StringSearching.java
@@ -1,28 +1,55 @@
 package Arrays.Searching.LinearSearching;
 
-public class StringSearching {
-
-    public static void main(String[] args) {
-        String str = "shiwank";
-        char target = 't';
-        System.out.println(search(str,target));
-    }
-
-    static boolean search(String str, char target){
-        for(int i=0; i<str.length(); i++){
-            if(target == str.charAt(i)){
-                return true;
-            }
-        }
-
-        //OR
-
+//public class StringSearching {
+//
+//    public static void main(String[] args) {
+//        String str = "shiwank";
+//        char target = 't';
+//        System.out.println(search(str,target));
+//    }
+//
+//    static boolean search(String str, char target){
+//        for(int i=0; i<str.length(); i++){
+//            if(target == str.charAt(i)){
+//                return true;
+//            }
+//        }
+//
+//        //OR
+//
 //        for(char e : str.toCharArray()){
 //            if(e == target){
 //                return true;
 //            }
 //        }
+//
+//        return false;
+//    }
+//}
 
-        return false;
+
+public class StringSearching {
+
+    public static void main(String[] args) {
+
+        String name = "Shiwank";
+        char target = 'w';
+
+        System.out.println("Character is present at :- " + searching(name,target));
+    }
+
+    static int searching(String name, char target){
+
+        if(name.length() == 0){
+            return -1;
+        }
+
+        for(int i=0; i<name.length(); i++ ){
+            if (target == name.charAt(i)){
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
