0febc02f70c0d7cd1615b5b7647d831b49cb493e
Shiwank <shiwannnnk@gmail.com>
2025-04-02 15:48:37 +0530

    completed types of inheritance

--- src/OOPS/Principles/Inheritance/Child.java
+++ src/OOPS/Principles/Inheritance/Child.java
@@ -2,10 +2,22 @@ package OOPS.Principles.Inheritance;
 
 public class Child extends Parent{
 
+    //although it is a child class but cannot access the private context of parent class
+
     double weight;
 
     public Child (){
         this.weight = 1;
+    }
+
+    public Child(Child other){
+        super(other);  //child class has context of parent class, thus it can happen
+        weight = other.weight;
+    }
+
+    public Child(double side, double weight){
+        super(15);
+        this.weight = 15;
     }
 
     public Child(double l, double h, double w, double weight){
--- src/OOPS/Principles/Inheritance/Main.java
+++ src/OOPS/Principles/Inheritance/Main.java
@@ -14,6 +14,19 @@ public class Main {
         Child obj3 = new Child(3,4,5,8);
         System.out.println(obj3.l);
 
+        Parent obj4 = new Child(2,4,5,10);
+//        System.out.println(obj4.weight);
+
+//        Child obj5 = new Parent(2,3,5);
+
+        Child obj6 = new Child(obj2);
+        System.out.println(obj6.l);
+
+        Child1 obj7 = new Child1(5,60);
+        System.out.println(obj7.price);
+
+
+
     }
 
 
