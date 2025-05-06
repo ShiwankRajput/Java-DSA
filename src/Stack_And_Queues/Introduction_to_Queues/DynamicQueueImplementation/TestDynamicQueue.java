d0f6924e28ef4e8e69eefeee5bb9632b1e17f3b3
Shiwank <shiwannnnk@gmail.com>
2025-05-06 08:31:45 +0530

    Testing Dynamic Queue

--- src/Stack_And_Queues/Introduction_to_Queues/DynamicQueueImplementation/TestDynamicQueue.java
+++ src/Stack_And_Queues/Introduction_to_Queues/DynamicQueueImplementation/TestDynamicQueue.java
@@ -1,4 +1,23 @@
 package Stack_And_Queues.Introduction_to_Queues.DynamicQueueImplementation;
 
 public class TestDynamicQueue {
+
+    public static void main(String[] args) throws Exception{
+
+        DynamicQueue queue = new DynamicQueue(5);
+
+        queue.insert(15);
+        queue.insert(17);
+        queue.insert(18);
+        queue.insert(19);
+        queue.insert(120);
+        queue.insert(11);
+
+        queue.display();
+        queue.remove();
+        queue.display();
+        System.out.println(queue.front());
+
+    }
+
 }
