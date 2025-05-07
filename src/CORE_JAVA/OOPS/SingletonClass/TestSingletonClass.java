f0a945218974ad0d756fa705fb3b133684ec02bc
Shiwank <shiwannnnk@gmail.com>
2025-05-07 11:37:21 +0530

    singleton class

---
+++ src/CORE_JAVA/OOPS/SingletonClass/TestSingletonClass.java
@@ -0,0 +1,29 @@
+package CORE_JAVA.OOPS.SingletonClass;
+
+class SingletonClass {
+
+    static SingletonClass obj = new SingletonClass();
+
+    private SingletonClass(){
+
+    }
+
+    public static SingletonClass getInstance(){
+        return obj;
+    }
+
+}
+
+class TestSingletonClass{
+
+    public static void main(String[] args) {
+
+        SingletonClass obj1 = SingletonClass.getInstance();
+        SingletonClass obj2 = SingletonClass.getInstance();
+
+        System.out.println(obj1);
+        System.out.println(obj2);
+
+    }
+
+}
--- src/CORE_JAVA/OOPS/StaticExample/StaticInnerClasses.java
+++ src/CORE_JAVA/OOPS/StaticExample/StaticInnerClasses.java
@@ -1,10 +1,13 @@
 package CORE_JAVA.OOPS.StaticExample;
 
-//outside classes cannot be static because it does not depends on other class, whereas innerClasses may
+//outside classes cannot be static because it does not depend on other class, whereas innerClasses may
 // or may not be static
 
 public class StaticInnerClasses {
 
+    // static here means inner class does not depend on instance of outer class, so if we create object of
+    // non-static inner class in main body it will give error as we need to create object of outer class
+    //so that it can run
     static class Test{
         String name;
 
