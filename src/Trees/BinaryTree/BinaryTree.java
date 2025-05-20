09cd944137af7f99ed2bf4fe332835f131b6e633
Shiwank <shiwannnnk@gmail.com>
2025-05-20 09:46:43 +0530

    Introduction to trees

---
+++ src/Trees/BinaryTree/BinaryTree.java
@@ -0,0 +1,21 @@
+package Trees.BinaryTree;
+
+public class BinaryTree {
+
+    public BinaryTree(){
+
+    }
+
+    private static class Node{
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value){
+            this.value = value;
+        }
+    }
+
+    private Node root;
+
+}
