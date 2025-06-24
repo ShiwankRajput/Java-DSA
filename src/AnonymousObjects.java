e91f8469054407512bcd457c06249616c919a510
Shiwank <shiwannnnk@gmail.com>
2025-06-24 12:15:14 +0530

    understanding anonymous objects

---
+++ src/AnonymousObjects.java
@@ -0,0 +1,26 @@
+class A{
+    public static void main(String[] args){
+
+        /*
+
+        Anonymous Objects ->
+
+         */
+
+        new B().f1();  // anonymous objects -> for accessing different methods of class B we need to
+        // create object again and again.
+        new B().f1();
+
+        new B().f2();
+
+    }
+}
+
+class B{
+    public void f1(){
+        System.out.println("B f1()");
+    }
+    public void f2(){
+        System.out.println("B f2()");
+    }
+}
