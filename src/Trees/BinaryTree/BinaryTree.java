92f432a6f244619f5bca83ba65928c0908888385
ShiwankRajput <shiwankrajput35@gmail.com>
2025-07-05 23:04:44 +0530

    intro to trees DS

--- src/Trees/BinaryTree/BinaryTree.java
+++ src/Trees/BinaryTree/BinaryTree.java
@@ -1,5 +1,7 @@
 package Trees.BinaryTree;
 
+import java.util.Scanner;
+
 public class BinaryTree {
 
     public BinaryTree(){
@@ -17,5 +19,34 @@ public class BinaryTree {
     }
 
     private Node root;
+
+    // insert elements ->
+    public void populate(Scanner input){
+        System.out.println("Enter the root node value : ");
+        root = new Node(input.nextInt());
+        populate(input, root);
+    }
+
+    private void populate(Scanner input, Node node){
+        System.out.println("Do you want ot enter to left of : " + node.value);
+        boolean left = input.nextBoolean();
+        if(left){
+            System.out.println("Enter the value of left node : ");
+            int value = input.nextInt();
+            node.left = new Node(value);
+            populate(input,node.left);
+        }
+
+        System.out.println("Do you want to enter to right of :" + node.value);
+        boolean right = input.nextBoolean();
+        if(right){
+            System.out.println("Enter the value of right node : ");
+            int value = input.nextInt();
+            node.right = new Node(value);
+            populate(input,node.right);
+        }
+    }
+
+
 
 }
