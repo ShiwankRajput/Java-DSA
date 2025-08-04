f718a1ffeb173b846f62cf73967beb78e9528d03
ShiwankRajput <shiwannnnk@gmail.com>
2025-08-04 14:53:16 +0530

    solvind some problems related to BFS

---
+++ src/Trees/Questions/Average_Of_Levels.java
@@ -0,0 +1,53 @@
+package Trees.Questions;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import javax.swing.tree.TreeNode;
+
+// https://leetcode.com/problems/average-of-levels-in-binary-tree/description/
+// 637 - Average of levels in Binary Tree
+
+public class Average_Of_Levels {
+
+    public List<Double> averageOfLevels(TreeNode root) {
+
+        List<Double> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+
+            double avg = 0;
+
+            int listSize = queue.size();
+
+            for (int i = 0; i < listSize; i++) {
+                TreeNode currentNode = queue.poll();
+                avg += currentNode.val;
+
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+
+            avg = avg / listSize;
+
+            result.add(avg);
+
+        }
+
+        return result;
+
+    }
+
+}
---
+++ src/Trees/Questions/Level_Order_Successor.java
@@ -0,0 +1,40 @@
+package Trees.Questions;
+
+import javax.swing.tree.TreeNode;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Level_Order_Successor {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public TreeNode successorNode(TreeNode root, int value){
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()){
+            TreeNode currentNode = queue.poll();
+            int found = currentNode.val;
+
+            if(currentNode.left != null){
+                queue.offer(currentNode.left);
+            }
+            if(currentNode.right != null){
+                queue.offer(currentNode.right);
+            }
+
+            if(found == value){
+                break;
+            }
+
+        }
+
+        return queue.peek();
+
+    }
+
+}
