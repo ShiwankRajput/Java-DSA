fc5b661c518fb8b3a430c7137957484b975771b6
ShiwankRajput <shiwannnnk@gmail.com>
2025-08-06 11:22:22 +0530

    deep dive into Trees

---
+++ src/Trees/Questions/BinaryTree_RightView.java
@@ -0,0 +1,52 @@
+package Trees.Questions;
+
+// https://leetcode.com/problems/binary-tree-right-side-view/
+// 199 - Binary Tree Right side view
+
+
+import javax.swing.tree.TreeNode;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTree_RightView {
+
+    public List<Integer> rightSideView(TreeNode root) {
+
+        List<Integer> result = new LinkedList<>();
+
+        if(root == null){
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()){
+
+            int levelSize = queue.size();
+
+            for (int i=0; i<levelSize; i++){
+
+                TreeNode currentNode = queue.poll();
+
+                if(i == levelSize-1){
+                    result.add(currentNode.val);
+                }
+
+                if(currentNode.left != null){
+                    queue.offer(currentNode.left);
+                }
+
+                if(currentNode.right != null){
+                    queue.offer(currentNode.right);
+                }
+
+            }
+        }
+
+        return result;
+
+    }
+
+}
