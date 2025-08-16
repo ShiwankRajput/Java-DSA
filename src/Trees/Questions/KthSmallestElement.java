27da3eba27be9739262181306f45ce8a14480311
ShiwankRajput <shiwannnnk@gmail.com>
2025-08-16 13:27:19 +0530

    more ques

---
+++ src/Trees/Questions/KthSmallestElement.java
@@ -0,0 +1,36 @@
+package Trees.Questions;
+
+// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+// 230 -> Kth Smallest Element in Binary Tree :
+
+import javax.swing.tree.TreeNode;
+
+public class KthSmallestElement {
+
+    public static void main(String[] args) {
+
+    }
+
+    int count = 0;
+    int result = -1;
+
+    public int kthSmallest(TreeNode root, int k) {
+        kthSmallestHelper(root, k);
+        return result;
+    }
+
+    private void kthSmallestHelper(TreeNode node, int k) {
+        if (node == null) return;
+
+        kthSmallestHelper(node.left, k);
+
+        count++;
+        if (count == k) {
+            result = node.val;
+            return;
+        }
+
+        kthSmallestHelper(node.right, k);
+    }
+
+}
