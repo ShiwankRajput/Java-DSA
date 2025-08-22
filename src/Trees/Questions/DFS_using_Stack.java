ebb72155d9704f6856d4721fcd7f15a2c53ab516
ShiwankRajput <shiwannnnk@gmail.com>
2025-08-22 15:10:48 +0530

    deep dive to questions

---
+++ src/Trees/Questions/DFS_using_Stack.java
@@ -0,0 +1,43 @@
+package Trees.Questions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class DFS_using_Stack {
+
+    // DFS using Stack :-
+    public void traverse(){
+
+        List<Integer> ansList = new ArrayList<>();
+
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+
+        while(!stack.isEmpty()){
+
+            Node topNode = stack.pop();
+            ansList.add(topNode.value);
+
+            if(topNode.right != null){
+                stack.push(topNode.right);
+            }
+            if(topNode.left != null){
+                stack.push(topNode.left);
+            }
+
+        }
+
+    }
+
+}
+
+
+class Main{
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
--- src/Trees/Questions/IsPathExist.java
+++ src/Trees/Questions/IsPathExist.java
@@ -1,7 +1,40 @@
 package Trees.Questions;
 
+// Is path exist from root to leaf :
+
+import javax.swing.tree.TreeNode;
+
 public class IsPathExist {
 
+    public static void main(String[] args) {
 
+    }
+
+    public boolean pathExist(){
+
+        int[] arr = {3,9,12,8};
+
+        return pathExist(root, arr, 0);
+    }
+
+    private boolean pathExist(TreeNode node, int[] arr, int i){
+
+        if(node == null){
+            return false;
+        }
+
+        if(i >= arr.length || arr[i] != node.val){
+            return false;
+        }
+
+        // checking for leaf node also while iterating over array as we have to find that path exist from root
+        // to left.
+        if((node.left == null && node.right == null) && i == arr.length-1){
+            return true;
+        }
+
+        return pathExist(node.left, arr, i+1) || pathExist(node.right, arr, i+1);
+
+    }
 
 }
